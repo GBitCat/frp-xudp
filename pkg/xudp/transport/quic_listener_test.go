87d81a59742aa72260d8b06cd2add5c1a33fb5a3
GBitCat <GBitCat@users.noreply.github.com>
2026-08-15 08:07:11 +0800

    test: verify QUIC listener close unblocks accept

---
+++ pkg/xudp/transport/quic_listener_test.go
@@ -0,0 +1,60 @@
+package transport
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestQUICListenerCloseUnblocksAccept(t *testing.T) {
+	t.Parallel()
+
+	serverConn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1")})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer serverConn.Close()
+
+	serverID, err := GenerateIdentity()
+	if err != nil {
+		t.Fatal(err)
+	}
+	clientID, err := GenerateIdentity()
+	if err != nil {
+		t.Fatal(err)
+	}
+	serverTLS, err := ServerTLSConfig(serverID, clientID.Fingerprint())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	listener, err := Listen(serverConn, serverTLS, Options{
+		KeepalivePeriod: time.Second,
+		MaxIdleTimeout:  5 * time.Second,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	acceptCh := make(chan error, 1)
+	go func() {
+		conn, err := listener.Accept(context.Background())
+		if err == nil {
+			_ = conn.Close()
+		}
+		acceptCh <- err
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+	_ = listener.Close()
+
+	select {
+	case err := <-acceptCh:
+		if err == nil {
+			t.Fatal("Accept returned nil after listener close")
+		}
+	case <-time.After(2 * time.Second):
+		t.Fatal("Accept remained blocked after listener close")
+	}
+}
